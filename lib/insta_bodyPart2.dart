8cb045ae154e50513b3e2ecf46bfb02616c44bc4
Yasas4D <yasassandeepa007@gmail.com>
2020-06-28 21:58:59 +0530

    Ui Improved

--- lib/insta_bodyPart2.dart
+++ lib/insta_bodyPart2.dart
@@ -20,130 +20,136 @@ class CategeoryCard extends StatelessWidget {
         //       MaterialPageRoute(builder: (context) => SubCategoery(documentID)));
       },
       child: Card(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 0.0, 8.0, 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      new Container(
-                        height: 40.0,
-                        width: 40.0,
-                        decoration: new BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: new DecorationImage(
-                              fit: BoxFit.fill,
-                              image: new NetworkImage(dc.data['user_image'])),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 4.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 0.0, 8.0, 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        new Container(
+                          height: 40.0,
+                          width: 40.0,
+                          decoration: new BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: new DecorationImage(
+                                fit: BoxFit.fill,
+                                image: new NetworkImage(dc.data['user_image'])),
+                          ),
                         ),
-                      ),
-                      new SizedBox(
-                        width: 10.0,
-                      ),
-                      new Text(
-                        dc.data['name'],
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                  new IconButton(
-                      icon: Icon(Icons.more_vert),
-                      onPressed: () async {
-                        print("Adding Data..!!!");
-                        // await Firestore.instance
-                        //     .collection('books')
-                        //     .document()
-                        //     .setData({'title': 'title', 'author': 'author'});
-                        // print("Done------------");
-                      }),
-                ],
+                        new SizedBox(
+                          width: 10.0,
+                        ),
+                        new Text(
+                          dc.data['contact_name'],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Flexible(
-              fit: FlexFit.loose,
-              child: new Image.network(
-                dc.data['image'],
-                fit: BoxFit.cover,
+              Flexible(
+                fit: FlexFit.loose,
+                child: new Image.network(
+                  dc.data['image'],
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      new Icon(
-                        FontAwesomeIcons.heart,
-                        color: Colors.red,
-                      ),
-                      new SizedBox(
-                        width: 16.0,
-                      ),
-                      new Icon(
-                        FontAwesomeIcons.comment,
-                        color: Colors.grey,
-                      ),
-                      new SizedBox(
-                        width: 16.0,
-                      ),
-                      new Icon(
-                        FontAwesomeIcons.paperPlane,
-                        color: Colors.grey,
-                      ),
-                    ],
-                  ),
-                  new Icon(
-                    FontAwesomeIcons.bookmark,
-                    color: Colors.blue,
-                  )
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        new SizedBox(
+                          width: 8.0,
+                        ),
+                        new Container(
+                          decoration: new BoxDecoration(
+                            borderRadius: new BorderRadius.circular(16.0),
+                            color: Colors.yellow[700],
+                          ),
+                          child: Text(dc.data['name'],
+                              style: TextStyle(
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black)),
+                        ),
+                        // new RaisedButton(
+                        //   onPressed: () {
+                        //     print("object");
+                        //   },
+                        //   color: Colors.yellow[700],
+                        // child: Text(dc.data['name'],
+                        //     style: TextStyle(
+                        //         fontSize: 15.0,
+                        //         fontWeight: FontWeight.bold,
+                        //         color: Colors.black)),
+                        // )
+                      ],
+                    ),
+                    new RaisedButton(
+                      onPressed: () {
+                        print("object");
+                      },
+                      color: Colors.blue,
+                      child: Text(dc.data['price'],
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                    )
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                dc.data['aim'],
-                style: TextStyle(fontWeight: FontWeight.bold),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  dc.data['description'],
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            // Padding(
-            //   padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.start,
-            //     children: <Widget>[
-            //       new Container(
-            //         height: 40.0,
-            //         width: 40.0,
-            //         decoration: new BoxDecoration(
-            //           shape: BoxShape.circle,
-            //           image: new DecorationImage(
-            //               fit: BoxFit.fill,
-            //               image: new NetworkImage(
-            //                   "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
-            //         ),
-            //       ),
-            //       new SizedBox(
-            //         width: 10.0,
-            //       ),
-            //       Expanded(
-            //         child: new TextField(
-            //           decoration: new InputDecoration(
-            //             border: InputBorder.none,
-            //             hintText: "Add a comment...",
-            //           ),
-            //         ),
-            //       ),
-            //     ],
-            //   ),
-            // ),
-          ],
+              // Padding(
+              //   padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+              //   child: Row(
+              //     mainAxisAlignment: MainAxisAlignment.start,
+              //     children: <Widget>[
+              //       new Container(
+              //         height: 40.0,
+              //         width: 40.0,
+              //         decoration: new BoxDecoration(
+              //           shape: BoxShape.circle,
+              //           image: new DecorationImage(
+              //               fit: BoxFit.fill,
+              //               image: new NetworkImage(
+              //                   "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+              //         ),
+              //       ),
+              //       new SizedBox(
+              //         width: 10.0,
+              //       ),
+              //       Expanded(
+              //         child: new TextField(
+              //           decoration: new InputDecoration(
+              //             border: InputBorder.none,
+              //             hintText: "Add a comment...",
+              //           ),
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
--- lib/insta_home.dart
+++ lib/insta_home.dart
@@ -1,4 +1,3 @@
-import 'package:Team_Furious/insta_body.dart';
 import 'package:Team_Furious/project_form/orderForm.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -67,13 +66,17 @@ class _InstaHomeState extends State<InstaHome> {
     backgroundColor: new Color(0xfff8faf8),
     centerTitle: true,
     elevation: 1.0,
-    leading: new Icon(Icons.camera_alt),
+    // leading: new Icon(Icons.camera_alt),
     title: SizedBox(
         height: 35.0, child: Image.asset("assets/images/insta_logo.png")),
     actions: <Widget>[
       Padding(
         padding: const EdgeInsets.only(right: 12.0),
-        child: Icon(Icons.send),
+        child: new IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () async {
+              print("Adding Data..!!!");
+            }),
       )
     ],
   );
